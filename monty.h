146a3899e97c5da0fd94b627eb200132eefd1af2
R6Doc <michaelgalindez274@gmail.com>
2020-05-14 14:29:13 -0500

    adding library updated

--- monty.h
+++ monty.h
@@ -11,7 +11,7 @@
 #include <fcntl.h>
 #include <ctype.h>
 
-extern int errcheck;
+extern int error_check; /** var to check errors */
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -24,13 +24,13 @@ extern int errcheck;
  */
 typedef struct stack_s
 {
-int n;
-struct stack_s *prev;
-struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
+ * struct instruction_s - opcoode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
@@ -39,8 +39,22 @@ struct stack_s *next;
  */
 typedef struct instruction_s
 {
-char *opcode;
-void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
-#ifndef
+void pall(stack_t **stack, __attribute__((unused)) unsigned int line);
+int get_opcode_func(char *opcode, stack_t **stack, unsigned int line_num);
+void push(stack_t **stack, unsigned int n, char *token);
+void free_dlistint(stack_t **head);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void free_all(stack_t *stack, char *line, FILE *fp);
+#endif
